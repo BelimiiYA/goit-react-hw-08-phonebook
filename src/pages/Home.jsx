5a3967d43239621063405ff296d6e18506976aa2
BelimiiYA <belim7vlad@gmail.com>
2023-09-11 12:11:23 +0200

    1.0

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import { Wrapper, Title } from './Home.styled';
+
+export default function Home() {
+  return (
+    <Wrapper>
+      <Title>Ласкаво просимо до Телефонної книги!</Title>{' '}
+    </Wrapper>
+  );
+}
